3ae0015c26f0f744fa3652d82f2158d1861299a4
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2024-03-18 20:33:47 -0300

    Simple CSV reader

---
+++ BtcRPC.Tests/Tools/CSV.cs
@@ -0,0 +1,11 @@
+﻿namespace BtcRPC.Tests.Tools;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+public static class CSV
+{
+    public static IEnumerable<string[]> LoadCSV(string path, char separator = ',')
+        => File.ReadLines(path).Select(l => l.Split(','));
+}
