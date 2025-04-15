3e768be3ef3517675db432e84807de4170105c9a
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-04-15 10:26:09 -0300

    Updates getmininginfo model to 29.0

--- Simple.BTC/Models/Mining/GetMiningInfo.cs
+++ Simple.BTC/Models/Mining/GetMiningInfo.cs
@@ -3,9 +3,20 @@
 public class GetMiningInfo_Result
 {
     public long blocks { get; set; }
+    public string bits { get; set; }
     public decimal difficulty { get; set; }
+    public string target { get; set; }
     public decimal networkhashps { get; set; }
     public long pooledtx { get; set; }
     public string chain { get; set; }
-    public string warnings { get; set; }
+    public Next next { get; set; }
+    public string[] warnings { get; set; }
+
+    public class Next
+    {
+        public int height { get; set; }
+        public string bits { get; set; }
+        public decimal difficulty { get; set; }
+        public string target { get; set; }
+    }
 }
